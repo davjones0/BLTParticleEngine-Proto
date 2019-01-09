a6b0dce5bc179d82333fdbc23beb45bf65118af6
David Jones <djones@eidexinsights.com>
2019-01-09 17:25:19 -0500

    emitter progress

--- main.py
+++ main.py
@@ -12,21 +12,25 @@ def run():
 
     terminal.refresh()
 
-    p1 = Particle(50, 25, 0, 10, 90, 1)
-    p2 = Particle(50, 25, 0, 10, 70, 1)
+    #p1 = Particle(50, 25, 0, 10, 90, 1)
+    #p2 = Particle(50, 25, 0, 10, 70, 1)
+    emitter = Emitter(25, 25, 1, 90, 2.0)
+    emitter.update(0)
 
-    sudo_pool = [p1, p2]
+    print(emitter._particlePool)
+    #sudo_pool = [p1, p2]
     timer1 = time.perf_counter()
     while terminal.read() != terminal.TK_CLOSE:
         terminal.clear()
-        for par in sudo_pool:
+        for par in emitter._particlePool:
+            print(par)
             render(par)
             timer2 = time.perf_counter()
             delta = timer2 - timer1
-            #print(delta)
-            par.update(delta)
+            # print(delta)
+            emitter.update(delta)
         pass
-    
+
     terminal.clear()
 
 
@@ -63,7 +67,7 @@ class Particle(object):
 
 
 class Emitter(object):
-    def __init__(self):
+    def __init__(self, x, y, speed, angle, emissionRate):
         self._particlePool = []
         self._particleCount = 0
         self._particleIndex = 0
@@ -72,19 +76,23 @@ class Emitter(object):
 
         self.totalParticles = 0
         self.emissionRate = 0
-        
+
         self.active = False
         self.duration = 0
 
+        self._pos = {
+            "x": x,
+            "y": y
+        }
         self.posVar = {
-            "x": posVar["x"],
-            "y": posVar["y"]
+            "x": 0,
+            "y": 0
         }
 
-        self.speed = 0
+        self.speed = speed
         self.speedVar = 0
 
-        self.angle = 0
+        self.angle = angle
         self.angleVar = 0
 
         self.life = 0
@@ -118,10 +126,61 @@ class Emitter(object):
 
     def _initParticle(self, particle):
         posVar = {
-            "x": self.posVar["x"] * random.randrange(-1,1),
-            "y": self.posVar["y"] * random.randrange(-1,1)
+            "x": self.posVar["x"] * random.randrange(-1, 1),
+            "y": self.posVar["y"] * random.randrange(-1, 1)
         }
 
-        
+        # if this.posVarTransformFn {
+        # posVar = this.posVarTransformFn(posVar, util)
+        # }
+
+        particle.position["x"] = posVar["x"] + self._pos["x"]
+        particle.position["y"] = posVar["y"] + self._pos["y"]
+        particle.position["z"] = 0
+
+        angle = self.angle + self.angleVar * random.randrange(-1, 1)
+        speed = self.speed + self.speedVar * random.randrange(-1, 1)
+
+        particle.setVelocity(angle, speed)
+        particle._startingLife = self.life + self.lifeVar * random.randrange(-1, 1)
+
+    def _updateParticle(self, particle, delta, i):
+        if particle.life > 0:
+            particle.forces["x"] = 0
+            particle.forces["y"] = 0
+
+            particle.position["x"] += particle.velocity["x"] * delta
+            particle.position["y"] += particle.velocity["y"] * delta
+
+            particle.life -= delta
+
+            ++self._particleIndex
+        else:
+            temp = self._particlePool[i]
+            self._particlePool[i] = self._particlePool[self._particleCount - 1]
+            self._particlePool[self._particleCount - 1] = temp
+            --self._particleCount
+
+    def update(self, delta):
+        print(delta)
+        self._elapsed += delta
+        self.active = self._elapsed < self.duration
+
+        if not self.active:
+            return
+
+        if self.emissionRate:
+            rate = 1.0 / self.emissionRate
+            self._emitCounter += delta
+
+            while not self._isFull() and self._emitCounter > rate:
+                self._addParticle()
+                self._emitCounter -= rate
+
+        self._particleIndex = 0
+
+        while self._particleIndex < self._particleCount:
+            p = self._particlePool[self._particleIndex]
+            self._updateParticle(p, delta, self._particleIndex)
 
 run()
